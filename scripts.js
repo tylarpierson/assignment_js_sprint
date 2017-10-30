b17897bc0ed3858cf13c90a34b35366d0d6def02
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2017-10-30 10:40:03 -0700

    Modified largestEl

--- scripts.js
+++ scripts.js
@@ -1,32 +1,33 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(){
+    let largestNumber = [3, 10, 1, 5];
+    console.log(Math.max(largestNumber));
   },
-  
-  reversed: function(){  
+
+  reversed: function(){
     // your code here
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(){
     // your code here
   },
 
-  compareArrays: function(){ 
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
